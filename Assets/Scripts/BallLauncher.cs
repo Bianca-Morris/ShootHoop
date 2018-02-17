fc7ef85cbedd2b47864b3230946537510ae394f1
Bianca-Morris <bmorris@wellesley.edu>
2018-02-16 16:13:27 -0800

    add basic controls

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	public float ballSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		print("Ball Spawner ready!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Shoot the ball with right mouse button
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject newBall = Instantiate (ballPrefab);
+			// Give velocity to the ball
+			newBall.transform.position = transform.position; // spawn at the camera player
+			Rigidbody rb = newBall.GetComponent<Rigidbody> ();
+			rb.velocity = Vector3.forward * ballSpeed;
+		}
+
+
+//		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+//			
+//		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+//			GameObject newBall = Instantiate (ballPrefab);
+//			// Give velocity to the ball
+//			Rigidbody rb = newBall.GetComponent<Rigidbody> ();
+//			rb.velocity = Vector3.left * speed;
+//		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+//			GameObject newBall = Instantiate (ballPrefab);
+//			// Give velocity to the ball
+//			Rigidbody rb = newBall.GetComponent<Rigidbody> ();
+//			rb.velocity = Vector3.right * speed;
+//		}
+	}
+}
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Player Rotate ready!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float rotationSpeed = 4.0f;
+
+		// Adjust camera X
+		float mouseX = Input.GetAxis ("Mouse X") * rotationSpeed;
+		transform.localRotation *= Quaternion.Euler (0, mouseX, 0);
+
+		// Adjust camera Y
+		float mouseY = Input.GetAxis ("Mouse Y") * rotationSpeed;
+		GetComponentInChildren<Camera>().transform.localRotation *= Quaternion.Euler (-mouseY, 0, 0);
+	
+	}
+}
